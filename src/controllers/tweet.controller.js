a7dfc8a0989f8455439a15a2b7d18071a86776ca
Atharva Awari <140247890+atharvaawari@users.noreply.github.com>
2024-09-25 00:54:52 +0530

    add update and getuser tweet controllers and routes for tweet

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -26,11 +26,55 @@ const createTweet = asyncHandler(async (req, res) => {
 
 });
 
-const getUserTweet = asyncHandler(async (req, res)=>{
+const getUserTweet = asyncHandler(async (req, res) => {
+  //Todo: get user tweet
+  const { userId } = req.params;
+
+  if (!userId) throw new ApiError(401, "Invalid userId");
+
+  const tweet = await Tweet.find({ owner: userId }).populate('owner');
+
+  if (!tweet.length) throw new ApiError(404, "No tweet found for these user");
+
+  return res
+    .status(200)
+    .json(
+      200,
+      tweet,
+      "Successfully fetched tweets for these user"
+    )
+});
+
+const updateTweet = asyncHandler(async (req, res) => {
+  //Todo: update tweet
+  const { content } = req.body;
+  const { tweetId } = req.params;
+
+  if(!content) throw new ApiError(400,"All fields are required");
   
+  if(!tweetId) throw new ApiError(400,"tweetId not found");
+  
+  const updatedTweet = await Tweet.findByIdAndUpdate( 
+    tweetId,
+    {
+      $set:{
+        content
+      }
+    },
+    {new: true} 
+  )
+
+    return res.status(200, updatedTweet, "Tweet updated successfully!");
+})
+
+const deleteTweet = asyncHandler(async (req, res) => {
+  //Todo: delete tweet
 })
 
 
 export {
   createTweet,
+  getUserTweet,
+  updateTweet,
+  deleteTweet
 }
--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -1,8 +1,19 @@
 import { Router } from "express";
 import { verifyJWT } from "../middleware/auth.middleware.js";
+import { 
+  createTweet, 
+  deleteTweet, 
+  getUserTweet, 
+  updateTweet 
+} from "../controllers/tweet.controller.js";
 
 const router = Router();
 router.use(verifyJWT);  //Apply verifyJWT middleware to all routes in this file
 
-router.route("/").post();
+router.route("/").post(createTweet);
+router.route("/user/:userId").get(getUserTweet);
+router.route("/:tweetId").patch(updateTweet).delete(deleteTweet);
 
+
+
+export default Router
