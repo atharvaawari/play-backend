2fb38fafc6af12295b77e4e10ac10c5a7e906733
Atharva Awari <140247890+atharvaawari@users.noreply.github.com>
2024-10-22 00:47:16 +0530

    added add video to playlist controller

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -4,6 +4,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { User } from "../models/user.model.js";
 import { Playlist } from "../models/playlist.model.js";
+import { Video } from "../models/video.model.js";
 
 
 
@@ -32,48 +33,12 @@ const getUserPlaylist = asyncHandler(async (req, res) => {
 
   if (!userId) throw new ApiError(401, "User not found");
 
-  const userPlaylist = await Playlist.aggregate([
+  const userPlaylist = await Playlist.find(
     {
       $match: {
         owner: new mongoose.Types.ObjectId(userId)
       }
-    },
-    {
-      $lookup: {
-        from: "users",
-        localField: "owner",
-        foreignField: "_id",
-        as: "owner"
-      },
-      pipeline: [
-        {
-          $project: {
-            userName: 1,
-            fullName: 1,
-            avatar: 1,
-          }
-        }
-      ]
-    },
-    {
-      $unwind: "$owner"
-    },
-    {
-      $lookup: {
-        from: "likes",
-        localField: "_id",
-        foreignField: "playlists",
-        as: "likes",
-        pipeline: [
-          {
-            $project: {
-              likeBy: 1
-            }
-          }
-        ]
-      }
-    },
-  ])
+    })
 
   if (!userPlaylist) throw new ApiError(401, "Failed to fetch playlist.");
 
@@ -85,16 +50,89 @@ const getUserPlaylist = asyncHandler(async (req, res) => {
 })
 
 const getPlaylistById = asyncHandler(async (req, res) => {
-  const { playlistId } = req.params
+  const { playlistId } = req.params;
   //TODO: get playlist by id
 
-  if(!playlistId) throw new ApiError(404, "No playlist found"); 
+  if (!playlistId) throw new ApiError(404, "No playlist found");
+
+  const playlist = Playlist.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(playlistId)
+      }
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "videos",
+        foreignField: "_id",
+        as: "videos",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    userName: 1,
+                    fullName: 1,
+                    avatar: 1,
+                  }
+                }
+              ]
+            }
+          },
+          {
+            $unwind: "$owner",
+          }
+        ]
+      }
+    }
+  ]);
+
+  if (!playlist) throw new ApiError(404, "Playlist not found");
+
+  res.status(200).json(
+    new ApiResponse(200, playlist, "Playlist fetched sucessed!")
+  );
 
 })
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
-  const { PlaylistId, videoId } = req.params
+  const { playlistId, videoId } = req.params
   // TODO: add video to the playlist
+
+  const playlist = await Playlist.findById(playlistId);
+
+  if (!playlist) throw new ApiError(404, "Playlist not found.");
+
+  const video = await Video.findById(videoId);
+
+  if (!video) throw new ApiError(404, "video not found.");
+
+  if (!playlist?.owner.equals(req.user?._id))
+    throw new ApiError(401, "Unauthorized request");
+
+  const updatedPlaylist = await Playlist.findByIdAndUpdate(
+    playlist,
+    {
+      $addToSet: {
+        videos: videoId
+      },
+    },
+    { new: true }
+  );
+
+  if (!updatedPlaylist) throw new ApiError(400, "Failed to add video to playlist");
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, updatedPlaylist, "Add video to playlist sucess!")
+    );
 })
 
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
