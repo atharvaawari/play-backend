33c6d427737e6c2b11edaf24e48e169ddb61dca2
Atharva Awari <140247890+atharvaawari@users.noreply.github.com>
2024-10-24 01:27:05 +0530

    added suubscription controller

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,13 +1,59 @@
-import mongoose , {isValidObjectId} from "mongoose";
+import mongoose, { isValidObjectId } from "mongoose";
 import { User } from "../models/user.model.js";
 import { Subscription } from "../models/subscription.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { subscribe } from "diagnostics_channel";
 
 
-const toggleSubscription = asyncHandler(async (req, res) =>{
+const toggleSubscription = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
-  
+  //TODO: toggle subscription
+
+  const channel = await User.findById(channelId);
+
+  if (!channel) throw new ApiError(404, "No channel found");
+
+  const isSubscribed = await Subscription.findOne({
+    subscriber: req.user._id,
+    channel: channelId
+  });
+
+  if (isSubscribed) {
+    await Subscription.findByIdAndDelete(isSubscribed._id);
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, { message: "Unsubscribed" }));
+
+  } else {
+    await Subscription.create({
+      subscriber: req.user?._id,
+      channel: channelId
+    })
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, { message: "Subscribed" }));
+  }
 
 })
+
+//controller to return subscriber list of a channel
+const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+  const { channelId } = req.params;
+
+})
+
+//controller to return channel list to which user has subscriber
+const getSubscribedChannels = asyncHandler(async (req, res) => {
+  const { subscriberId } = req.params;
+
+})
+
+export {
+  toggleSubscription,
+  getUserChannelSubscribers,
+  getSubscribedChannels
+}
--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -3,7 +3,6 @@ import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import { checkOwnership } from "../utils/checkOwnership.js";
 import mongoose from "mongoose";
 
 const createTweet = asyncHandler(async (req, res) => {
